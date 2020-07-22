c9c16a2e26973748f5a04fe6afadd291cbc2537d
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-07-22 13:24:35 +0700

    Update Api.java

--- AlQuran/app/src/main/java/com/azhar/quran/networking/Api.java
+++ AlQuran/app/src/main/java/com/azhar/quran/networking/Api.java
@@ -10,4 +10,7 @@ public class Api {
     public static String URL_LIST_SURAH = "https://api.npoint.io/99c279bb173a6e28359c/data";
     public static String URL_LIST_AYAT = "https://api.npoint.io/99c279bb173a6e28359c/surat/{nomor}";
 
+    //public static String URL_LIST_SURAH = "https://al-quran-8d642.firebaseio.com/data.json?print=pretty";
+    //public static String URL_LIST_AYAT = "https://al-quran-8d642.firebaseio.com/surat/{nomor}.json?print=pretty";
+
 }
